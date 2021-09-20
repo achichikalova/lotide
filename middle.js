bc1df317388eb1103f0fc10f0bc5f9cbbb29fa01
Antonina Chichikalova <tonia.chichikalova@gmail.com>
2021-09-20 17:31:12 +0000

    Modify middle and add test file to it

--- middle.js
+++ middle.js
@@ -1,22 +1,3 @@
-// TEST/ASSERTION FUNCTIONS
-const assertArraysEqual = function(actual, expected) {
-  if (eqArrays(actual, expected)) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-const eqArrays = function(actual, expected) {
-  for (let i = 0; i < actual.length; i++) {
-    if (actual[i] !== expected[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-// ACTUAL FUNCTION
 const middle = function(array) {
   let result = [];
   if (array.length <= 2) {
@@ -32,8 +13,4 @@ const middle = function(array) {
   return result;
 };
 
-
-//TEST CODE
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
-assertArraysEqual(middle([1, 2]), []);
+module.exports = middle;
