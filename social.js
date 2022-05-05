0213a0aaeea5ca25a7a4e66843c7d32f63f4648d
Antonina Chichikalova <tonia.chichikalova@gmail.com>
2022-05-05 17:20:29 -0400

    implement mostPopular function

--- social.js
+++ social.js
@@ -37,7 +37,7 @@ const biggestFollower = (users) => {
 
   for (let user of Object.keys(users)) {
     if (result) {
-      if (users[user].follows.length > result?.follows.length) {
+      if (users[user].follows.length > result.follows.length) {
         result = users[user];
       }
     } else {
@@ -45,23 +45,41 @@ const biggestFollower = (users) => {
     }
   }
 
-  return result.name;
+  return `Biggest follower is ${result.name}`;
 };
 
 console.log(biggestFollower(data));
 
-// Instruction
 // Implement mostPopular() which returns the name of the most popular (most followed) individual.
+const mostPopular = (users) => {
+  let followed = {};
 
-// Instruction
+  for (let user of Object.keys(users)) {
+    let followers = users[user].follows;
+    for (let follower of followers) {
+      followed[follower] = followed[follower] + 1 || 1;
+    }
+  }
+
+  const max = Object.keys(followed).filter((x) => {
+    return followed[x] == Math.max.apply(null, Object.values(followed));
+  });
+  const winners = max.map((winner) => {
+    return users[winner].name;
+  });
+  return `More followers has ${winners.join(", ")}`;
+};
+
+console.log(mostPopular(data));
+
 // Implement printAll() which outputs a list of everyone and for each of them, the names of who they follow and who follows them.
 
-// Instruction
 // Implement unrequitedFollowers() which returns a list of names for those who follow someone that doesn't follow them back.
 
-// Instruction
 // Implement some or all these remaining functions.
 
 // Identify who has the most followers over 30
+
 // Identify who follows the most people over 30
+
 // List everyone and their reach (sum of # of followers and # of followers of followers)
